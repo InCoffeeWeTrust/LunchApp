4dc9a6069fb7552dcfe8fd4936e683a62a2edbf7
dhd08001 <dhd08001@student.mdh.se>
2013-11-06 15:56:37 +0100

    Updated SingleMenuActivity

--- src/se/mdh/dva217/incoffeewetrust/SingleMenuActivity.java
+++ src/se/mdh/dva217/incoffeewetrust/SingleMenuActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
 import android.widget.TextView;
-//import se.mdh.dva217.incoffeewetrust.db.DatabaseHelper;
 
 /**
  * Created with IntelliJ IDEA.
@@ -15,37 +14,12 @@ import android.widget.TextView;
  */
 public class SingleMenuActivity extends Activity {
 
-    /*
-    String schoolName;
-    TextView school;
-    TextView monday;
-    TextView tuesday;
-    TextView wednesday;
-    TextView thursday;
-    TextView friday;
-
-    DatabaseHelper db;
-      */
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.singlemenu);
 
-        //bind all controls
-        /*
-        school = (TextView) findViewById(R.id.singleMenuSchoolname);
-        monday = (TextView) findViewById(R.id.singleMenuMondayLunch);
-        tuesday = (TextView) findViewById(R.id.singleMenuTuesdayLunch);
-        wednesday = (TextView) findViewById(R.id.singleMenuWednesdayLunch);
-        thursday = (TextView) findViewById(R.id.singleMenuThursdayLunch);
-        friday = (TextView) findViewById(R.id.singleMenuFridayLunch);
-          */
-        //create databasehelper with this context
-        //db = new DatabaseHelper(this);
-
-        // get schoolname, passed with intent
         TextView school = (TextView) findViewById(R.id.singleMenuSchoolname);
         school.setText(getIntent().getExtras().getString("schoolName"));
 
@@ -54,21 +28,11 @@ public class SingleMenuActivity extends Activity {
         setLunchText(R.id.singleMenuWednesdayLunch, 2);
         setLunchText(R.id.singleMenuThursdayLunch, 3);
         setLunchText(R.id.singleMenuFridayLunch, 4);
-
-        //get the menu from the sqlite db
-        //String[] menu = db.getMenu(schoolName,0);
-
-        //set text for all textviews
-        /*
-        monday.setLunchText(menu[2]);
-        tuesday.setLunchText(menu[3]);
-        wednesday.setLunchText(menu[4]);
-        thursday.setLunchText(menu[5]);
-        friday.setLunchText(menu[6]);
-        */
     }
 
     private void setLunchText(int id, int index) {
+
+
         TextView textView = (TextView) findViewById(id);
         textView.setText(getIntent().getExtras().getStringArray("menu")[index]);
     }
