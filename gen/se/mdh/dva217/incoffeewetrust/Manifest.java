c794eed766ff4ac904c773e82a3009400952915f
tln02003 <tln02003@student.mdh.se>
2013-11-06 16:31:17 +0100

    Deleting gen folder

--- gen/se/mdh/dva217/incoffeewetrust/Manifest.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package se.mdh.dva217.incoffeewetrust;
-
-/* This stub is for using by IDE only. It is NOT the Manifest class actually packed into APK */
-public final class Manifest {
-}
